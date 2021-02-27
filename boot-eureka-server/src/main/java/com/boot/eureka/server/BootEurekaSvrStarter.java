6ffd516512237097c8fbf394606b877d80252806
qipingzhiji <1213462884@qq.com>
2021-02-27 20:28:58 +0800

    Initial commit

---
+++ boot-eureka-server/src/main/java/com/boot/eureka/server/BootEurekaSvrStarter.java
@@ -0,0 +1,14 @@
+package com.boot.eureka.server;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class BootEurekaSvrStarter {
+	public static void main(String[] args) {
+		SpringApplication.run(BootEurekaSvrStarter.class, args);
+	}
+}
